5404e07e8ee6fcabf2028ccc6886ceed8bc81c24
qliphoth <paradox.qliphoth@gmail.com>
2020-04-03 13:26:54 +0700

    remove django

--- django_api/frontend/CompanyModel/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from CompanyModel.models import Dbdcompany, DbdQuery, DbdNewQuery
-
-# Register your models here.
-admin.site.register([Dbdcompany, DbdQuery, DbdNewQuery])
--- django_api/frontend/upload/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from upload import views
- 
-urlpatterns = [
-    url(r'^upload/', views.load_file)
-]
